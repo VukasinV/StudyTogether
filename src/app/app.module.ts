48b929d8351d533041c80186c5139badec54abcb
lanagrozda <lanagrozdanovic@gmail.com>
2018-05-10 16:27:25 +0200

    porukica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ApiService } from './api.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
     HeaderComponent,
     FooterComponent,
     WelcomeComponent,
+    ProfileComponent,
 
   ],
   imports: [
